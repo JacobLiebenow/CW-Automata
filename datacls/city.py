ff225a7e8f36f28ebe5df0c52692a3ec735172f8
JacobLiebenow <37095936+JacobLiebenow@users.noreply.github.com>
2018-03-20 17:04:05 -0400

    Create city.py

---
+++ datacls/city.py
@@ -0,0 +1,33 @@
+#Written by: Jacob S Liebenow
+
+from datacls import venue
+
+#The city is the primary unit of organization, containing the most data. Cities are
+#organized into states, but the cities themselves contain venues, bands, etc.
+
+class City:
+	
+	venueNumber = 0
+
+	#Each city consists of its own name, venues, bands (to be added), and promoters/bookers (to be added)
+	def __init__(self, cityName, venues=None):
+		self.cityName = cityName
+		if venues is None:
+			self.venues = []
+		else:
+			self.venues = venues
+		
+	#The following 3 functions are self explanatory by title - add, remove, or print venues
+	def addVenue(self, venue):
+		if venue not in self.venues:
+			self.venues.append(venue)
+			self.venueNumber += 1
+			
+	def removeVenue(self, venue):
+		if venue in self.venues:
+			self.venues.remove(venue)
+			self.venueNumber -= 1
+			
+	def printVenues(self):
+		for venue in self.venues:
+			print("----->", venue.venueName)
