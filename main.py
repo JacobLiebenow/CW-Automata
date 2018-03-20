b3bef27b17025d70c012f9e2fabd24a52d9875e2
JacobLiebenow <37095936+JacobLiebenow@users.noreply.github.com>
2018-03-19 22:59:49 -0400

    Create main.py
    Primary file to run software

---
+++ main.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import httplib2
+import os
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from datacls import *
+from datacls.calendar import *
+from datacls.location import *
+from datacls.contact import *
+
+def main(): 
+    pass
+
+if __name__ == '__main__':
+    main()
