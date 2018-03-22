0a1fb359e3b10d880492a571975494be9dc7176e
JacobLiebenow <37095936+JacobLiebenow@users.noreply.github.com>
2018-03-22 17:27:43 -0400

    Update venue.py

--- datacls/venue.py
+++ datacls/venue.py
@@ -1,5 +1,7 @@
 #Written by: Jacob S Liebenow
 #
+#
+#
 #Each venue has an associated name, address, bands, and promoters that frequent the establishment.
 #
 #DevNote(3/20/2018, 11:48AM EST): I have to wonder if it would be more efficient to organize this into 
@@ -7,9 +9,35 @@
 #would make the modules themselves more messy.  Something to weigh, although I'm inclined to continue down
 #the multi-module route.  It's good OOP practice if nothing else, and what I'm used to from Java.
 
+from datacls import datacenter
+from datacls import state
+from datacls import city
+from datacls import contact
+from datacls import band
+
 class Venue:
 	
 	#Each venue has associated bands and promoters/bookers...
 	#...however those bands and bookers aren't tied solely to a specific venue, or even a city or state
-	def __init__(self, venueName):
+	def __init__(self, venueName, roles=None, managers=None):
 		self.venueName = venueName
+		
+		if roles is None:
+			self.roles = []
+		else: 
+			self.roles = roles
+		
+		if managers is None:
+			self.managers = []
+		else:
+			self.managers = managers
+		
+	
+	def addManager(self, roleName, contact):
+	
+		if roleName not in self.roles:
+			contact.addManagerRole(self.venueName, roleName)
+			self.roles.append(roleName)
+		
+		if contact not in self.managers:	
+			self.managers.append(contact)
